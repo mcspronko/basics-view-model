8805fcff9ab1878dcc4149efe352b5bed639df2b
Max Pronko <max.pronko@gmail.com>
2021-12-08 13:47:47 +0000

    Removed controller logic

--- Controller/Post/Index.php
+++ Controller/Post/Index.php
@@ -4,32 +4,15 @@ declare(strict_types=1);
 
 namespace MageMastery\BasicsViewModel\Controller\Post;
 
-use Magento\Framework\App\Action\Context;
 use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\Controller\ResultInterface;
-use Magento\Framework\View\Result\PageFactory;
 use Magento\Framework\App\Action\Action;
 
 class Index extends Action implements HttpGetActionInterface
 {
-    private $pageFactory;
-
-    public function __construct(Context $context, PageFactory $pageFactory)
-    {
-        $this->pageFactory = $pageFactory;
-        parent::__construct($context);
-    }
-
     public function execute(): ResultInterface
     {
-//        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
-        $page = $this->pageFactory->create();
-        $page->addHandle('some_random_handle');
-
-        $page->getLayout()->getBlock('magemastery.basics.handles')
-            ->setData('handles', $page->getLayout()->getUpdate()->getHandles());
-
-        return $page;
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
